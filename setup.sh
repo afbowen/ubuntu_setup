565b0a0a81cc4620c258b889953a4e3c92a9104c
Adam Bowen <adam@bowen.com>
2025-04-30 11:24:16 -0700

    Update setup.sh, Add Package Config Directory - Add apt packages to install - Add pkg-cfg/ for puTTY config

--- setup.sh
+++ setup.sh
@@ -4,18 +4,18 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-
-echo "[1/9] Updating system..."
+echo "[1/11] Updating system..."
 sudo apt update && sudo apt upgrade -y
 
 
 printf "\n"
-echo "[2/9] Installing APT packages..."
-sudo apt install -y git curl tree python3-pip gnome-tweaks gnome-shell-extensions gnome-shell-extension-prefs gnome-shell-extension-manager stow kate numlockx
+echo "[2/11] Installing APT packages..."
+sudo apt install -y git curl tree python3-pip gnome-tweaks gnome-shell-extensions gnome-shell-extension-prefs gnome-shell-extension-manager stow kate numlockx putty grip net-tools minicom
 
 
 printf "\n"
-echo "[3/9] Installing .dev packages..."
+echo "[3/11] Installing .dev packages..."
+echo "Installing Google Chrome..."
 if ! command -v google-chrome &>/dev/null; then
   echo "Installing Google Chrome..."
   wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -25,7 +25,9 @@ else
   echo "Google Chrome already installed."
 fi
 
+
 printf "\n"
+echo "Installing Slack..."
 if ! command -v slack &>/dev/null; then
   echo "Installing Slack..."
   wget -O /tmp/slack.deb https://downloads.slack-edge.com/desktop-releases/linux/x64/4.41.96/slack-desktop-4.41.96-amd64.deb
@@ -37,7 +39,7 @@ fi
 
 
 printf "\n"
-echo "[4/9] Setting up dotfiles..."
+echo "[4/11] Setting up dotfiles..."
 cd dotfiles
 DOTFILES_DIR="$SCRIPT_DIR/dotfiles"
 TARGET_DIR="$HOME"
@@ -47,25 +49,31 @@ cd ..
 
 
 printf "\n"
-echo "[5/9] Loading GNOME keybindings..."
+echo "[5/11] Loading GNOME keybindings..."
 dconf load /org/gnome/desktop/wm/keybindings/ < my-dconf-backups/desktop-wm-keybindings.conf
 dconf load /org/gnome/shell/keybindings/ < my-dconf-backups/shell-keybindings.conf
 dconf load /org/gnome/shell/extensions/dash-to-dock/ < my-dconf-backups/shell-extensions-dash-to-dock-keybindings.conf
 
 
 printf "\n"
-echo "[6/9] Installing GNOME extensions..."
+echo "[6/11] Installing GNOME extensions..."
 pip3 install gnome-extensions-cli
-gnome-extensions-cli install putWindow@clemens.lab21.org
+echo yes | gnome-extensions-cli install putWindow@clemens.lab21.org
 gnome-extensions-cli install wsmatrix@martin.zurowietz.de
+# mkdir -p ~/.local/share/gnome-shell/extensions/
 cp gnome_extensions/org.gnome.shell.extensions.org-lab21-putwindow.gschema.xml ~/.local/share/gnome-shell/extensions/putWindow@clemens.lab21.org/schemas
 cp gnome_extensions/org.gnome.shell.extensions.wsmatrix.gschema.xml ~/.local/share/gnome-shell/extensions/wsmatrix@martin.zurowietz.de/schemas
 glib-compile-schemas ~/.local/share/gnome-shell/extensions/putWindow@clemens.lab21.org/schemas/
 glib-compile-schemas ~/.local/share/gnome-shell/extensions/wsmatrix@martin.zurowietz.de/schemas/
+# gnome-extensions enable putWindow@clemens.lab21.org
+# gnome-extensions enable wsmatrix@martin.zurowietz.de
+
+# gsettings set org.gnome.shell.extensions.dash-to-dock shortcut "['<Super>\`']"
+# ~/.config/dconf/user
 
 
 printf "\n"
-echo "[7/9] Applying GNOME settings..."
+echo "[7/11] Applying GNOME settings..."
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
@@ -75,14 +83,33 @@ gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
 
 printf "\n"
-echo "[8/9] Setting up /etc files..."
+echo "[8/11] Setting up /etc files..."
 cd etc
-sudo cp systemd/logind.conf /etc/systemd/logind.conf
+sudo cp systemd/logind.conf /etc/systemd/logind.conf  # Lid when docked settings
 cd ..
 
 
 printf "\n"
-echo "[9/9] Generating ed25519 SSH key if it doesn't exist..."
+echo "[9/11] Setting up /etc files..."
+numlockx on
+cd pkg_cfg
+cp putty/sessions/* ~/.putty/sessions/
+cd ..
+
+
+printf "\n"
+echo "[9/11] Cloning this repository to ~/git/ubuntu_setup..."
+mkdir ~/git/
+git clone git@github.com:afbowen/ubuntu_setup.git
+
+
+printf "\n"
+echo "[10/11] Creating ~/shared directory for LAN file transfers..."
+mkdir ~/shared
+
+
+printf "\n"
+echo "[11/11] Generating ed25519 SSH key if it doesn't exist..."
 
 SSH_KEY="$HOME/.ssh/id_ed25519"
 
@@ -93,14 +120,15 @@ else
   echo "SSH key already exists at $SSH_KEY"
 fi
 
-echo
+printf "\n"
 echo "Your public SSH key:"
 cat "${SSH_KEY}.pub"
 echo
 
 printf "\n"
 echo "Opening GitHub for you to manually enter your new SSH key..."
-google-chrome "https://github.com/settings/ssh/new"
+google-chrome "https://github.com/settings/ssh/new" &
+
 
 printf "\n"
 read -p "🎉 Setup complete! Would you like to reboot now? (y/n): " REBOOT_CHOICE
